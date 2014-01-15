9287e6f07b8d7be7b905161f99e35e16921f3162
yw2 <yw2@ualberta.ca>
2014-01-14 19:30:32 -0700

    TweetListModel

---
+++ src/ca/ualberta/cs/lonelytwitter/TweetListModel.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.ArrayList;
+
+public class TweetListModel
+{
+	private ArrayList<LonelyTweetModel> list;
+
+	public ArrayList<LonelyTweetModel> getList()
+	{
+	
+		return list;
+	}
+
+	
+	public void setList(ArrayList<LonelyTweetModel> list)
+	{
+	
+		this.list = list;
+	}
+	
+}
