cfdab4a3a601cc3db24110afc68e3a4f3dcb69a5
yw2 <yw2@ualberta.ca>
2014-01-14 17:46:24 -0700

    Constructor overloading

--- src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -1,7 +1,42 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.Date;
 
 public class lonelyTweetModel
 {
+	private String text;
+	private Date timestamp;
+	
+	public String getText()
+	{
+	
+		return text;
+	}
+	
+	public void setText(String text)
+	{
+	
+		this.text = text;
+	}
+	
+	public Date getTimestamp()
+	{
+	
+		return timestamp;
+	}
+	
+	public void setTimestamp(Date timestamp)
+	{
+	
+		this.timestamp = timestamp;
+	}
 
+	public lonelyTweetModel(String text)
+	{
+
+		super();
+		this.text = text;
+		this.timestamp = new Date();
+	}
+	
 }
