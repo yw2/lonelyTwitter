ecc1e4bee9cf38bd56889db8e9f192b9789b62d3
yw2 <yw2@ualberta.ca>
2014-01-28 17:48:10 -0700

    Add test project

---
+++ lonelyTwitterTest/src/ca/ualberta/cs/lonelytwitter/test/LonelyTweetModelTest.java
@@ -0,0 +1,21 @@
+package ca.ualberta.cs.lonelytwitter.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity;
+
+
+public class LonelyTweetModelTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity>
+{
+
+	public LonelyTweetModelTest(String name)
+	{
+
+		super(LonelyTwitterActivity.class);
+	}
+	
+	public void testFailure() {
+		
+		assertEquals("5 should equal 5", 5, 5);
+	}
+
+}
